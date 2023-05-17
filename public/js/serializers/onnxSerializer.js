9675787e2d193b0522eca7abee16a8a998abff41
cyberg00se <zhuk.bohdana@gmail.com>
2023-05-17 03:31:08 +0100

    refactor onnx serializer

--- public/js/serializers/onnxSerializer.js
+++ public/js/serializers/onnxSerializer.js
@@ -6,7 +6,7 @@ async function serializeONNXModel(model) {
             console.log('Serializing ONNX model:');
             const onnxProto = await loadProtoDefinition("js/lib/onnx/onnx.proto");
             const root = protobuf.Root.fromJSON(onnxProto);
-
+            
             const ModelProto = root.lookupType("onnx.ModelProto");
             const GraphProto = root.lookupType("onnx.GraphProto");
             const NodeProto = root.lookupType("onnx.NodeProto");
@@ -19,58 +19,63 @@ async function serializeONNXModel(model) {
             const rawGraph = GraphProto.create();
             rawModel.graph = rawGraph;
 
-            const mainNodes = model.nodes.filter(node => node.type !== "Input" && node.type !== "Output");
-            rawGraph.node = mainNodes.map((node) => {
+            const createAttribute = (key, value) => {
+                const rawAttr = AttributeProto.create();
+                rawAttr.name = key;
+                if (Array.isArray(value)) {
+                    if (value.every((val) => Number.isInteger(val))) {
+                        rawAttr.ints = value;
+                        rawAttr.type = AttributeProto.AttributeType.INTS;
+                    } else if (value.every((val) => typeof val === "number")) {
+                        rawAttr.floats = value;
+                        rawAttr.type = AttributeProto.AttributeType.FLOATS;
+                    } else if (value.every((val) => typeof val === "string")) {
+                        rawAttr.strings = value;
+                        rawAttr.type = AttributeProto.AttributeType.STRINGS;
+                    } else if (value.every((val) => typeof val === "boolean")) {
+                        rawAttr.ints = value.map((val) => (val ? 1 : 0));
+                        rawAttr.type = AttributeProto.AttributeType.INTS;
+                    }
+                } else {
+                    if (typeof value === "boolean") {
+                        rawAttr.i = value ? 1 : 0;
+                        rawAttr.type = AttributeProto.AttributeType.INT;
+                    } else if (Number.isInteger(value)) {
+                        rawAttr.i = value;
+                        rawAttr.type = AttributeProto.AttributeType.INT;
+                    } else if (typeof value === "number") {
+                        rawAttr.f = value;
+                        rawAttr.type = AttributeProto.AttributeType.FLOAT;
+                    } else if (typeof value === "string") {
+                        rawAttr.s = value;
+                        rawAttr.type = AttributeProto.AttributeType.STRING;
+                    }
+                }
+                return rawAttr;
+            }   
+            
+            const createNode = (node) => {
                 const rawNode = NodeProto.create();
-        
+          
                 rawNode.name = node.name;
                 rawNode.opType = node.type;
                 rawNode.input = node.inputs;
                 rawNode.output = node.outputs;
-
-                rawNode.attribute = Object.entries(node.attributes).map(([key, value]) => {
-                    const rawAttr = AttributeProto.create();
-                    rawAttr.name = key;
-                    if (Array.isArray(value)) {
-                        if (value.every((val) => Number.isInteger(val))) {
-                            rawAttr.ints = value;
-                            rawAttr.type = AttributeProto.AttributeType.INTS;
-                        } else if (value.every((val) => typeof val === "number")) {
-                            rawAttr.floats = value;
-                            rawAttr.type = AttributeProto.AttributeType.FLOATS;
-                        } else if (value.every((val) => typeof val === "string")) {
-                            rawAttr.strings = value;
-                            rawAttr.type = AttributeProto.AttributeType.STRINGS;
-                        } else if (value.every((val) => typeof val === "boolean")) {
-                            rawAttr.ints = value.map((val) => (val ? 1 : 0));
-                            rawAttr.type = AttributeProto.AttributeType.INTS;
-                        }
-                    } else {
-                        if (typeof value === "boolean") {
-                            rawAttr.i = value ? 1 : 0;
-                            rawAttr.type = AttributeProto.AttributeType.INT;
-                        } else if (Number.isInteger(value)) {
-                            rawAttr.i = value;
-                            rawAttr.type = AttributeProto.AttributeType.INT;
-                        } else if (typeof value === "number") {
-                            rawAttr.f = value;
-                            rawAttr.type = AttributeProto.AttributeType.FLOAT;
-                        } else if (typeof value === "string") {
-                            rawAttr.s = value;
-                            rawAttr.type = AttributeProto.AttributeType.STRING;
-                        }
-                    }
-                    return rawAttr;
-                });
-
+                rawNode.attribute = Object.entries(node.attributes).map(([key, value]) =>
+                    createAttribute(key, value)
+                );
+          
                 return rawNode;
-            });
+            };
 
-            const inputNodes = model.nodes.filter(node => node.type === "Input");
-            rawGraph.input = inputNodes.map((node) => {
+            rawGraph.node = model.nodes
+                .filter((node) => node.type !== "Input" && node.type !== "Output")
+                .map(createNode);
+
+            const createValueInfo = (node) => {
                 const rawValueInfo = ValueInfoProto.create();
                 rawValueInfo.name = node.name;
-        
+
                 const tensorProto = TensorProto.create();
                 tensorProto.elemType = OnnxDataType[node.attributes.elemType];
                 tensorProto.shape = TensorShapeProto.create();
@@ -79,33 +84,27 @@ async function serializeONNXModel(model) {
                 if (node.attributes.content) {
                     //const initData = node.attributes.content;
                     const initData = [];
-
+                
                     const initTensor = TensorProto.create();
                     initTensor.dims = node.attributes.shape;
                     initTensor.name = node.name;
                     initTensor.dataType = OnnxDataType[node.attributes.elemType];
                     initTensor.rawData = initData;
-
+                
                     rawGraph.initializer.push(initTensor);
                 }
-        
+                
                 rawValueInfo.type = { tensorType: tensorProto };
                 return rawValueInfo;
-            });
+            };
 
-            const outputNodes = model.nodes.filter(node => node.type === "Output");
-            rawGraph.output = outputNodes.map((node) => {
-                const rawValueInfo = ValueInfoProto.create();
-                rawValueInfo.name = node.name;
-        
-                const tensorProto = TensorProto.create();
-                tensorProto.elemType = OnnxDataType[node.attributes.elemType];
-                tensorProto.shape = TensorShapeProto.create();
-                tensorProto.shape.dim = node.attributes.shape.map(value => { return { dimValue: value} });
-        
-                rawValueInfo.type = { tensorType: tensorProto };
-                return rawValueInfo;
-            });
+            rawGraph.input = model.nodes
+                .filter((node) => node.type === "Input")
+                .map(createValueInfo);
+
+            rawGraph.output = model.nodes
+                .filter((node) => node.type === "Output")
+                .map(createValueInfo);
 
             console.log(rawModel);
 
